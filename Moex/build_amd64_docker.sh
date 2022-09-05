65c76e5fbf0f2aece9775d909bd50e2d71d38eaf
AProV666 <95475774+AProV666@users.noreply.github.com>
2022-09-05 21:42:18 +0300

    Delete Mo1

--- Moex/build_amd64_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/amd64 --push -t aprov/historyquotes:latest . -f Dockerfile-amd64
--- Moex/src/main/java/com/pro/springapp/service/MoexHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.pro.springapp.service;
-
-import com.pro.springapp.model.TickerPojo;
-
-import java.util.List;
-
-public interface MoexHistoryService {
-    List<TickerPojo> getHistory(String ticker);
-}
